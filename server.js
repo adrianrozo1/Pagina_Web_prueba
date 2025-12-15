b4703a3059b13d51977263ec3e812048b8f66bf6
unknown <albertorozo.estudiante@iser.edu.co>
2025-12-15 00:28:56 -0500

    API funcionando

--- server.js
+++ server.js
@@ -5,18 +5,10 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 
-const db = mysql.createPool({
-  host: 'mysql',
-  user: 'camarasuser',
-  password: 'PASSWORD',
-  database: 'camarasdb'
-});
-
 app.get('/data', (req, res) => {
-  db.query('SELECT * FROM camaras', (err, rows) => {
-    if (err) return res.status(500).json(err);
-    res.json(rows);
-  });
+  res.json({ mensaje: "API funcionando" });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('API corriendo en puerto 3000');
+});
